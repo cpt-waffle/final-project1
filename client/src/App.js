14d1ebb2d6a565542d8af1aa19077cb9c29acd56
vasiliy-klimkin <vasily.klimkin@gmail.com>
2023-06-08 12:08:54 -0400

    display memes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
+import { useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [memes, setMemes] = useState([]);
+
+
+  useEffect(() => {
+    axios.get('/memes').then(res => {
+      setMemes(res.data);
+    })
+  }, [])
+
+
   return (
     <div className="App">
+      <h1>Memes</h1>
+      {memes.map(meme => <article key={meme.id}>
+        <img src={meme.img}/>
+      </article>)}
     </div>
   );
 }
